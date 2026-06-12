541bc2a9738ae22b1bb6bfb6394ed90036f092c1
Baevin632 <martinindia99@gmail.com>
2026-06-13 00:36:16 +0530

    final changes

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ print("BEFORE EMBEDDINGS")
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 embeddings = GoogleGenerativeAIEmbeddings(
-    model="models/embedding-004",
+    model="models/text-embedding-004",
     google_api_key=os.getenv("key")
 )
 print("AFTER EMBEDDINGS")
