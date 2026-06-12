18286d6942d494ddf5e2c04b77daa05804f56db3
Baevin632 <martinindia99@gmail.com>
2026-06-13 00:59:04 +0530

    final changes -2

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 embeddings = GoogleGenerativeAIEmbeddings(
     model="models/gemini-embedding-001",
-    google_api_key=os.getenv("key")
 )
 print("AFTER EMBEDDINGS")
 
